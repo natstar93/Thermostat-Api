65891e2348dd1479ae3c1b9e7acf0b43aded8350
Natalie Akam <natalie.akam@gmail.com>
2015-07-09 22:03:20 +0100

    Initial commit

---
+++ app.rb
@@ -0,0 +1,11 @@
+require 'sinatra/base'
+
+class ThermostatApi < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+
+  run! if app_file == $0
+
+  set :views, proc { File.join(root, 'views')}
+end
